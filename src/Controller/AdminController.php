ca4708ec88565f4e12dfb6a13b1ae38350d3513d
Aymen Bohli <bohliaymen5@gmail.com>
2020-01-16 17:52:04 +0100

    login

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+/**
+ * @isGranted("ROLE_ADMIN")
+ */
 
 class AdminController extends AbstractController
 {
@@ -12,6 +18,8 @@ class AdminController extends AbstractController
      */
     public function index()
     {
+      //  $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         return $this->render('admin/index.html.twig');
     }
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route("/front/", name="front")
+ */
 class DefaultController extends AbstractController
 {
     /**
